93d4c9ad81f88ccb73a4eee1fd6b00f0773bb4b4
Yuval Aboulafia <yuval.abou@gmail.com>
2020-05-25 12:44:45 +0300

    Added CPU stats

--- clock.py
+++ clock.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 
 import spidev
@@ -6,24 +5,32 @@ from time import sleep
 from vfd import VFD
 import datetime
 
+import psutil
+
 vfd = VFD(0,0)
 displaySize = 20
 
 print("<==== Clock v0.2 ====>")
 
 vfd.setCursor(0, 0)
-thistext = "Clock v0.2"
-vfd.text(thistext.center(displaySize))
-sleep(1)
+welcome = "Clock v0.2"
+vfd.text(welcome.center(displaySize))
+sleep(2)
 
 print("<==== Starting Clock ====>")
 
 try:
     vfd.clear()
     while True:
+
         now = datetime.datetime.now()
         clock = (now.strftime("%H:%M:%S"))
+        cpu_temperature = psutil.sensors_temperatures()['cpu-thermal'][0].current
+        cpu_state = str(int(psutil.cpu_freq().current)) + " MHz" + " " + str(round(cpu_temperature, 1)) + "°C"
+
         vfd.text(clock.center(displaySize))
+        vfd.setCursor(0, 1)
+        vfd.text(cpu_state.center(displaySize))
         sleep(0.1)
         vfd.home()
 finally:
